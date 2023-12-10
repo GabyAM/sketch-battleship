4e01ef17e3d40a0c00791f6b3ef00b44bf4d7385
GabyAM <gdgabyam@gmail.com>
2023-12-10 10:20:33 -0300

    adds test for pubsub module
    tests that a function can correctly subscribe and unsubscribe from an event

---
+++ src/tests/pubsub.test.js
@@ -0,0 +1,9 @@
+it("pubsub module methods test", () => {
+	const testFunction = jest.fn();
+	pubsub.subscribe("valueUpdated", testFunction);
+	pubsub.publish("valueUpdated", "hi!");
+	expect(testFunction).toHaveBeenCalledWith("hi!");
+	pubsub.unsubscribe("valueUpdated", testFunction);
+	pubsub.publish("valueUpdated", "bye!");
+	expect(testFunction).not.toHaveBeenCalledWith("bye!");
+});
