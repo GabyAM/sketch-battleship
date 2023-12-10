344721cfff479907f0f8c54760bfb83e51603b5f
GabyAM <gdgabyam@gmail.com>
2023-12-10 08:45:37 -0300

    adds GameBoard class
    this class defines an empty board with methods to modify it's content

--- src/game.js
+++ src/game.js
@@ -12,3 +12,43 @@ export class Ship {
 		return this.hitsReceived === this.length;
 	}
 }
+export class GameBoard {
+	constructor(size) {
+		this.board = Array(size);
+		for (let i = 0; i < size; i++) {
+			this.board[i] = Array(size).fill(null);
+		}
+	}
+
+	placeShip(row, col, length) {
+		if (row > this.board.length || col > this.board.length) {
+			throw new Error("");
+		}
+		if (this.board[row][col] === null) {
+			if (row + length <= this.board.length) {
+				// adds the ship to each cell
+				const newShip = new Ship(length);
+				for (let i = 0; i < length; i++) {
+					this.board[row + i][col] = newShip;
+				}
+			}
+		}
+	}
+
+	receiveHit(row, col) {
+		if (row > this.board.length || col > this.board.length) {
+			throw new Error("");
+		}
+		if (this.board[row][col] instanceof Ship) {
+			this.board[row][col].hit();
+		} else {
+			this.board[row][col] = false;
+		}
+	}
+
+	isShipLeft() {
+		return this.board.forEach((col) => {
+			col.some((cell) => cell instanceof Ship);
+		});
+	}
+}
