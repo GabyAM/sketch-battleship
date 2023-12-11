686a69684632cb3173840bc5538706aed2f16df0
GabyAM <gdgabyam@gmail.com>
2023-12-11 11:04:13 -0300

    initializes app in index.js
    this makes everything initiate as expected. Will be modified later

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import { domController } from "./dom.js";
+import { gameController } from "./game.js";
+import { addBoardEvent } from "./eventListeners.js";
+import { pubsub } from "./pubsub.js";
 
+pubsub.subscribe("boardRendered", addBoardEvent);
+gameController.init();
